6e5d42e5def341576d647bbeb920db57c33b2bf7
joao Carmelio <jcg24708@gmail.com>
2024-12-01 18:55:41 -0300

    adicionado e atualizado - JoaoC

---
+++ app/src/main/java/com/example/nutri/activ_atividadef.java
@@ -0,0 +1,19 @@
+package com.example.nutri;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.nutri.R;
+
+public class activ_atividadef extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.atvidadef_tabela2);
+    }
+}
+
