042d99967aa7deba116073bb76746952f75b17a2
iarley <iarleymordekay@gmail.com>
2024-12-02 00:36:22 -0300

    Telas do Profissional ok

---
+++ app/src/main/java/com/example/nutri/activ_hidratacao_t.java
@@ -0,0 +1,20 @@
+package com.example.nutri;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.nutri.R;
+
+public class activ_hidratacao_t extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hidratacao_tabela);
+    }
+}
+
+
